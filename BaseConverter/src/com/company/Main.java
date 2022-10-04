8424e2cbeffc4dcbb1c55c6dd68c74225b7ccc04
Serhii <106743687+DenSerhii@users.noreply.github.com>
2022-10-04 16:29:46 +0300

    add new file

---
+++ BaseConverter/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //test
+        double temperature = 23.5;
+//        System.out.println("t = " +
+//                new CelsiusConverter().getConvertedValue(temperature));
+//        System.out.println("t = " +
+//                new KelvinConverter().getConvertedValue(temperature));
+//        System.out.println("t = " +
+//                new FahrenheitConverter().getConvertedValue(temperature));
+
+    }
+}
